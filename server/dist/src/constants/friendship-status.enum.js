72687be988b01be02d0b8577ac752527435537e8
mhanifiisik <62074009+mhanifiisik@users.noreply.github.com>
2023-07-10 12:48:33 +0200

    updated dependencies

--- server/dist/src/constants/friendship-status.enum.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var FriendshipStatus;
-(function (FriendshipStatus) {
-  FriendshipStatus['PENDING'] = 'pending';
-  FriendshipStatus['ACCEPTED'] = 'accepted';
-  FriendshipStatus['REJECTED'] = 'rejected';
-})(FriendshipStatus || (FriendshipStatus = {}));
-exports.default = FriendshipStatus;
--- server/dist/src/constants/user-status.enum.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var UserStatus;
-(function (UserStatus) {
-  UserStatus['ONLINE'] = 'online';
-  UserStatus['OFFLINE'] = 'offline';
-})(UserStatus || (UserStatus = {}));
-exports.default = UserStatus;
